48d5ad4f929c1360c523ae8332b598843ce9f934
Corey <walcocor@mail.gvsu.edu>
2024-03-18 01:06:57 -0400

    updated from shawns app.vue from outside src. missing logic to change beverage.vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,71 @@
 <template>
   <div>
     <Beverage :isIced="currentTemp === 'Cold'" />
-    <ul>
-      <li>
-        <template v-for="temp in temps" :key="temp">
-          <label>
-            <input
-              type="radio"
-              name="temperature"
-              :id="`r${temp}`"
-              :value="temp"
-              v-model="currentTemp"
-            />
-            {{ temp }}
-          </label>
-        </template>
-      </li>
-    </ul>
-  </div>
-  <div>
-    <Syrup :Syrups="selectedSyrup" />
-    <div>
-      <template v-for="temp in syrups" :key="temp">
-          <label>
-            <input
-              type="radio"
-              name="syrup"
-              :id="`r${temp}`"
-              :value="temp"
-              v-model="selectedSyrup"
-            />
-            {{ temp }}
-          </label>
-        </template>
+    <div class="options">
+      <label>Temperature:</label>
+      <template v-for="temp in temps" :key="temp">
+        <label>
+          <input
+            type="radio"
+            name="temperature"
+            :id="`r${temp}`"
+            :value="temp"
+            v-model="currentTemp"
+          />
+          {{ temp }}
+        </label>
+      </template>
+    </div>
+
+    <!-- Creamer options -->
+    <div class="options">
+      <label>Creamer:</label>
+      <template v-for="creamer in creamers" :key="creamer">
+        <label>
+          <input
+            type="radio"
+            name="creamer"
+            :id="`r${creamer}`"
+            :value="creamer"
+            v-model="selectedCreamer"
+          />
+          {{ creamer }}
+        </label>
+      </template>
+    </div>
+
+    <!-- Syrup options -->
+    <div class="options">
+      <label>Syrup:</label>
+      <template v-for="syrup in syrups" :key="syrup">
+        <label>
+          <input
+            type="radio"
+            name="syrup"
+            :id="`r${syrup}`"
+            :value="syrup"
+            v-model="selectedSyrup"
+          />
+          {{ syrup }}
+        </label>
+      </template>
+    </div>
+
+    <!-- Base beverage options -->
+    <div class="options">
+      <label>Base Beverage:</label>
+      <template v-for="base in baseBeverages" :key="base">
+        <label>
+          <input
+            type="radio"
+            name="base"
+            :id="`r${base}`"
+            :value="base"
+            v-model="selectedBase"
+          />
+          {{ base }}
+        </label>
+      </template>
     </div>
   </div>
 </template>
@@ -40,12 +73,22 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Beverage from "./components/Beverage.vue";
-import Syrup from "./components/Syrup.vue";
+
 // Define reactive data
 const temps = ref(["Hot", "Cold"]);
 const currentTemp = ref("Hot");
-const syrups = ref(["Vanilla", "Caramel", "Hazelnut"])
-const selectedSyrup = ref("Vanilla");
+
+// Creamer options
+const creamers = ref(["None", "Milk", "Cream", "Half & Half"]);
+const selectedCreamer = ref("None");
+
+// Syrup options
+const syrups = ref(["None", "Vanilla", "Caramel", "Hazelnut"]);
+const selectedSyrup = ref("None");
+
+// Base beverage options
+const baseBeverages = ref(["Coffee", "Green Tea", "Black Tea"]);
+const selectedBase = ref("Coffee");
 </script>
 
 <style lang="scss">
@@ -61,5 +104,8 @@ html {
 }
 ul {
   list-style: none;
+}
+.options {
+  margin-bottom: 1rem;
 }
 </style>
