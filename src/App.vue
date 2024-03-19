85f96401ecb1ab1d2afa4e7b1ee3859c10027d43
ShawnC24 <69652935+ShawnC24@users.noreply.github.com>
2024-03-18 23:29:56 -0400

    made changes to app.vue
    made changes to app.vue so that it will work with part 2 and 3

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,86 @@
 <template>
   <div>
-    <Beverage :isIced="currentTemp === 'Cold'" />
-    <div class="options">
-      <label>Temperature:</label>
-      <template v-for="temp in temps" :key="temp">
-        <label>
-          <input
-            type="radio"
-            name="temperature"
-            :id="`r${temp}`"
-            :value="temp"
-            v-model="currentTemp"
-          />
-          {{ temp }}
-        </label>
-      </template>
-    </div>
-
-    <!-- Creamer options -->
-    <div class="options">
-      <label>Creamer:</label>
-      <template v-for="creamer in creamers" :key="creamer">
-        <label>
-          <input
-            type="radio"
-            name="creamer"
-            :id="`r${creamer}`"
-            :value="creamer"
-            v-model="selectedCreamer"
-          />
-          {{ creamer }}
-        </label>
-      </template>
-    </div>
-
-    <!-- Syrup options -->
-    <div class="options">
-      <label>Syrup:</label>
-      <template v-for="syrup in syrups" :key="syrup">
-        <label>
-          <input
-            type="radio"
-            name="syrup"
-            :id="`r${syrup}`"
-            :value="syrup"
-            v-model="selectedSyrup"
-          />
-          {{ syrup }}
-        </label>
-      </template>
-    </div>
-
-    <!-- Base beverage options -->
-    <div class="options">
-      <label>Base Beverage:</label>
-      <template v-for="base in baseBeverages" :key="base">
-        <label>
-          <input
-            type="radio"
-            name="base"
-            :id="`r${base}`"
-            :value="base"
-            v-model="selectedBase"
-          />
-          {{ base }}
-        </label>
-      </template>
-    </div>
+    <Beverage :isIced="currentTemp === 'Cold'"
+    :creamer="currentCreamer"
+    :syrup="currentSyrup"
+    :beverage="currentBaseBeverage" />
+    <ul>
+      <li>
+        <label>Temperature: </label>
+        <template v-for="temp in temps" :key="temp">
+          <label>
+            <input
+              type="radio"
+              name="temperature"
+              :id="`r${temp}`"
+              :value="temp"
+              v-model="currentTemp"
+            />
+            {{ temp }}
+          </label>
+        </template>
+      </li>
+      <li>
+        <label>Creamer: </label>
+        <template v-for="creamer in creamers" :key="creamer">
+          <label>
+            <input
+              type="radio"
+              name="creamer"
+              :id="`r${creamer}`"
+              :value="creamer"
+              v-model="currentCreamer"
+            />
+            {{ creamer }}
+          </label>
+        </template>
+      </li>
+      <li>
+        <label>Syrup: </label>
+        <template v-for="syrup in syrups" :key="syrup">
+          <label>
+            <input
+              type="radio"
+              name="syrup"
+              :id="`r${syrup}`"
+              :value="syrup"
+              v-model="currentSyrup"
+            />
+            {{ syrup }}
+          </label>
+        </template>
+      </li>
+      <li>
+       <label>Base: </label>
+        <template v-for="beverage in baseBeverages" :key="beverage">
+          <label>
+            <input
+              type="radio"
+              name="beverage"
+              :id="`r${beverage}`"
+              :value="beverage"
+              v-model="currentBaseBeverage"
+            />
+            {{ beverage }}
+          </label>
+        </template>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import Beverage from "./components/Beverage.vue";
-
 // Define reactive data
 const temps = ref(["Hot", "Cold"]);
 const currentTemp = ref("Hot");
-
-// Creamer options
 const creamers = ref(["None", "Milk", "Cream", "Half & Half"]);
-const selectedCreamer = ref("None");
-
-// Syrup options
+const currentCreamer = ref("None");
 const syrups = ref(["None", "Vanilla", "Caramel", "Hazelnut"]);
-const selectedSyrup = ref("None");
-
-// Base beverage options
+const currentSyrup = ref("None");
 const baseBeverages = ref(["Coffee", "Green Tea", "Black Tea"]);
-const selectedBase = ref("Coffee");
+const currentBaseBeverage = ref("Coffee");
 </script>
 
 <style lang="scss">
@@ -104,8 +96,5 @@ html {
 }
 ul {
   list-style: none;
-}
-.options {
-  margin-bottom: 1rem;
 }
 </style>
